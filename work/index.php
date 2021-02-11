80a8f9edefc985bc1d4331af0c831096e189df91
yuichi oyama <yuichi19991208@gmail.com>
2021-02-11 23:01:41 +0900

    add htmlspecialchars()

--- work/index.php
+++ work/index.php
@@ -3,7 +3,7 @@
 $n = mt_rand(1, 3);
 
 if ($n === 1) {
-  $message = 'UI/UXデザイナー見習いです';
+  $message = '<script>alert(1);</script>UI/UXデザイナー見習いです';
 } elseif ($n === 2) {
   $message = 'プログラミング勉強中です';
 } else {
@@ -28,7 +28,7 @@ if ($n === 1) {
       </div>
       <div class="info">
         <h1>山田太郎</h1>
-        <p><?= $message; ?></p>
+        <p><?= htmlspecialchars($message, ENT_QUOTES, 'UTF-8'); ?></p>
         <ul>
           <li>
             <a href="https://dotinstall.com" target="_blank">
