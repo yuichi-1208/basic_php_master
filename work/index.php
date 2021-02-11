0003f0e487314123a3dd5323c15fb2df251a5c9f
yuichi oyama <yuichi19991208@gmail.com>
2021-02-11 23:40:37 +0900

    重複している部分を共通化

--- work/index.php
+++ work/index.php
@@ -1,52 +1,8 @@
 <?php
 
-$n = mt_rand(1, 3);
-
-if ($n === 1) {
-  $message = 'UI/UXデザイナー見習いです';
-} elseif ($n === 2) {
-  $message = 'プログラミング勉強中です';
-} else {
-  $message = "phpちょっとわかります";
-}
+include('_header.php');
 
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title>太郎のポートフォリオサイト</title>
-  <link rel="icon" href="favicon.ico">
-  <meta name="description" content="太郎のポートフォリオサイトです。">
-  <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-  <header>
-    <div class="container">
-      <div class="icon">
-        <img src="img/taro.png" width="120" height="120" alt="太郎のアイコンです">
-      </div>
-      <div class="info">
-        <h1>山田太郎</h1>
-        <p><?= htmlspecialchars($message, ENT_QUOTES, 'UTF-8'); ?></p>
-        <ul>
-          <li>
-            <a href="https://dotinstall.com" target="_blank">
-              <img src="img/blog.png" width="20" height="20" alt="ブログサイトへのリンク画像です">
-            </a>
-          </li>
-          <li>
-            <a href="https://dotinstall.com" target="_blank">
-              <img src="img/photos.png" width="20" height="20" alt="写真サイトへのリンク画像です">
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </header>
-
-  <section class="works">
-    <h1>WORKS</h1>
     <section>
       <img src="img/work1.png" width="400" height="260" alt="勇者ゲームの紹介画像">
       <h1>勇者ゲーム</h1>
@@ -67,11 +23,8 @@ if ($n === 1) {
       <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
       <a href="memory.php">もっと見る</a>
     </section>
+<?php
 
-  </section>
+include('_footer.php');
 
-  <footer>
-    <p>(c) dotinstall.com</p>
-  </footer>
-</body>
-</html>
+?>
